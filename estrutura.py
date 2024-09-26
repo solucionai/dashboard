951048e384ac3a3be46c9168a6e4c7c867dcaeb8
solucionai <compartilhadosolucionai@gmail.com>
2024-09-26 08:53:13 -0300

    Update estrutura.py

--- estrutura.py
+++ estrutura.py
@@ -932,5 +932,5 @@ def update_tempo_por_lead_graph(selected_problema):
 
 # Rodar o aplicativo
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
 
