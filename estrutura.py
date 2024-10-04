dd0462c15079d0984c962bdf1b19e0d301cbae86
solucionai <compartilhadosolucionai@gmail.com>
2024-10-04 10:41:07 -0300

    Update estrutura.py

--- estrutura.py
+++ estrutura.py
@@ -1723,7 +1723,5 @@ def display_page(n_home, n_leads, n_atendentes):
             ])
 
 
-# Rodar o aplicativo
 if __name__ == "__main__":
-    app.run_server(debug=True)
-
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
